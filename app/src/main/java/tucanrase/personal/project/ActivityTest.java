8e2b70c8e6e26fbc416d29415b8578714db2a37b
TucanRase <tommy.karlsen@hotmail.es>
2022-03-13 20:21:40 +0000

    Added new fragments to add a bottom navigation bar. Now every color references colors.xml

---
+++ app/src/main/java/tucanrase/personal/project/ActivityTest.java
@@ -0,0 +1,44 @@
+package tucanrase.personal.project;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentTransaction;
+
+import android.os.Bundle;
+
+import tucanrase.personal.project.databinding.ActivityTestBinding;
+import tucanrase.personal.project.fragments.HomeFragment;
+import tucanrase.personal.project.fragments.SearchFragment;
+
+public class ActivityTest extends AppCompatActivity {
+ActivityTestBinding binding;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        binding=ActivityTestBinding.inflate(getLayoutInflater());
+        setContentView(binding.getRoot());
+
+        binding.bottomNavBar.setOnItemSelectedListener(item -> {
+            switch (item.getItemId()){
+                case R.id.searchNav:
+                    replaceFragment(new SearchFragment());
+                    break;
+                case R.id.homeNav:
+                    replaceFragment(new HomeFragment());
+                    break;
+            }
+
+            return true;
+        });
+    }
+
+    private void replaceFragment(Fragment fragment){
+
+        FragmentManager fragmentManager=getSupportFragmentManager();
+        FragmentTransaction fragmentTransaction=fragmentManager.beginTransaction();
+        fragmentTransaction.replace(R.id.frame_layout,fragment);
+        fragmentTransaction.commit();
+
+    }
+}
